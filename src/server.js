db2b2a6df1f9747a969f51d7adefcd02a648c6c9
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2022-03-24 14:17:25 -0700

    changed app.listen to use environment variable if present

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 import express from "express";
-import * as fs from "fs";
 
 const app = express();
 
-app.listen("5000");
+app.listen(process.env.PORT || 5000);
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Hello from the server!</h1>")
