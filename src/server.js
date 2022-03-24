87018709844803dfee60bb4f527f34951b78077f
Ryan Cahela <6194542+RyanCahela@users.noreply.github.com>
2022-03-24 14:43:06 -0700

    fixed filepath for fs.readFile in '/' route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import fs from 'fs';
 
 const app = express();
 
 app.listen(process.env.PORT || 5000);
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello from the server!</h1>")
+  fs.readFile("./src/data.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      const jsonData = JSON.parse(data);
+      res.status(200).json(jsonData);
+    }
+  });
 });
 
 
